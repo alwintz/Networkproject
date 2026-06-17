7b5c2ad89d1cf3b29f008ee523b76f07d02e964b
Miguel Angelo Maiopue Guilovica <miguelangelomg2004@gmail.com>
2026-06-17 23:00:00 +0200

    Update user_service.py
    Adding a simple password based authentication  method with signup to login and direct login to user services. The password was hashed using a salt to produce different results to same output

--- services/user_service.py
+++ services/user_service.py
@@ -1,5 +1,55 @@
+    
+from hashlib import pbkdf2_hmac
+import os
 class UserService:
+    
+   registered_users = {}
 
+   @staticmethod
+   def signUp(username, password):
+     if username in UserService.registered_users:
+      return "ERROR: Username already taken"
+     
+     if not username or not username.strip():
+        return "ERROR: Username cannot be empty"
+     
+     if not password or not password.strip():
+        return "ERROR: Password cannot be empty"
+     
+     if len(password) < 4:
+        return "ERROR: Password must be at least 4 characters"
+     
+     if "|" in password:
+        return "ERROR: Password cannot contain '|' character"
+    
+     salt = os.urandom(32)
+     key = pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100_000)
+     UserService.registered_users[username] = {'salt':salt, 'key':key}
+
+     print(f"[AUTH] New user registered: {username}")
+     return "SUCCESS"
+      
+
+   @staticmethod
+   def LogIn (username, password): 
+     if not username or not username.strip():
+        return "ERROR: Username cannot be empty"
+     
+     if not password or not password.strip():
+        return "ERROR: Password cannot be empty"
+     
+     if username not in UserService.registered_users:
+        return "ERROR: Username or password incorrect"
+     
+     new_salt = UserService.registered_users[username]['salt']
+     new_key = pbkdf2_hmac('sha256', password.encode('utf-8'), new_salt, 100_000)
+
+     if new_key != UserService.registered_users[username]['key']:
+      return "ERROR: Username or password incorrect"
+     
+     print(f"[AUTH] User logged in: {username}")
+     return 'SUCCESS'
+       
     @staticmethod
     def get_username(clients, client_socket):
 
