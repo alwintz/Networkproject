924c127966da4b875168b06e27dbf6cc4cc58e2b
Miguel Angelo Maiopue Guilovica <miguelangelomg2004@gmail.com>
2026-06-18 00:10:58 +0200

    Update server.py
    Added some comments, changed main thread so it only accepts connections and creates dedicated threads and some other small changes

--- server.py
+++ server.py
@@ -4,13 +4,49 @@ from services.user_service import UserService
 from services.room_service import RoomService
 from services.message_service import MessageService
 
+# ============================================================
+# SERVER CONFIGURATION
+# ============================================================
+# HOST = 0.0.0.0 means listen on all network interfaces
+# PORT identifies the chat service
+# MAX_USERS_PER_ROOM limits the number of users per room
+
 HOST = "0.0.0.0"
 PORT = 5000
-MAX_USERS_PER_ROOM = 3
+MAX_USERS_PER_ROOM = 10
+
+
+# ============================================================
+# CREATE TCP SERVER SOCKET
+# ============================================================
+# AF_INET     -> IPv4 addressing
+# SOCK_STREAM -> TCP protocol
+#
+# TCP provides:
+# - Reliable delivery
+# - Ordered packets
+# - Error detection
+# - Connection-oriented communication
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Turn on re-use of port
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                  
+# Bind server to IP address and port
 server.bind((HOST, PORT))
+
+# Start listening for client connections
 server.listen()
+
+
+# ============================================================
+# DATA STRUCTURES
+# ============================================================
+# clients         -> connected users
+# rooms           -> room membership
+# user_rooms      -> current room of each client
+# message_history -> stores messages for each room
 
 clients = []
 
@@ -29,8 +65,50 @@ message_history = {
 print(f"Server running on port {PORT}")
 print("Waiting for clients...")
 
+# ============================================================
+# CLIENT HANDLER THREAD
+# ============================================================
+# Receives clients and adds it to room 1 by default after authentication is a success
+# Processes all commands received from a client.
+#
+# Supported commands:
+# SHOW_USERS
+# SHOW_ROOMS
+# SWITCH: |<room_name>
+# HISTORY: |<room_name>
+# CHANGE_USERNAME:<new_username>
 
-def handle_client(client_socket):
+#<Any other message> - broadcast to current room
+
+def handle_client(client_socket, client_address):
+   # adding each client connection 
+    clients.append({                 # 
+        "socket": client_socket,
+        "username": username,
+        "ip": client_address[0],
+        "port": client_address[1]
+    })
+
+    rooms["Room1"].append(client_socket)
+    user_rooms[client_socket] = "Room1"
+
+    print(
+        f"{username} connected from "
+        f"{client_address[0]}:{client_address[1]}"
+    )
+
+    print(f"{username} joined Room1")
+
+    client_socket.send(
+        "You joined Room1 by default".encode()
+    )
+
+    MessageService.send_history_to_client(
+        message_history,
+        client_socket,
+        "Room1"
+    )
+    
     while True:
         try:
             message = client_socket.recv(1024).decode()
@@ -106,6 +184,7 @@ def handle_client(client_socket):
         except:
             break
 
+    # when the while True loop stops remove the client from clients and close connection
     UserService.remove_client(
         clients,
         rooms,
@@ -114,48 +193,27 @@ def handle_client(client_socket):
     )
 
 
+
+# ============================================================
+# MAIN SERVER LOOP
+# ============================================================
+# Waits for incoming client connections.
+#
+# For each new client:
+# 1. Accept connection
+# 2. Start a dedicated thread for each connection
+#
+# Threading allows the server to handle multiple clients simultaneosly without having
+# to worry about the accept() blocking
+
 while True:
+  try:
     client_socket, client_address = server.accept()
-
-    username = client_socket.recv(1024).decode()
-
-    if len(rooms["Room1"]) >= MAX_USERS_PER_ROOM:
-        client_socket.send(
-            "ERROR: Room1 is full. Try again later.".encode()
-        )
-        client_socket.close()
-        continue
-
-    clients.append({
-        "socket": client_socket,
-        "username": username,
-        "ip": client_address[0],
-        "port": client_address[1]
-    })
-
-    rooms["Room1"].append(client_socket)
-    user_rooms[client_socket] = "Room1"
-
-    print(
-        f"{username} connected from "
-        f"{client_address[0]}:{client_address[1]}"
-    )
-
-    print(f"{username} joined Room1")
-
-    client_socket.send(
-        "You joined Room1 by default".encode()
-    )
-
-    MessageService.send_history_to_client(
-        message_history,
-        client_socket,
-        "Room1"
-    )
-
     thread = threading.Thread(
         target=handle_client,
-        args=(client_socket,)
+        args=(client_socket, client_address),   # to pass the arguments to the handle client each time thread starts
+        daemon=True # when the main loop is stopped the thread will also be stopped
     )
-
     thread.start()
+  except Exception as e:
+    print(f"Client not accepted: {e}")
