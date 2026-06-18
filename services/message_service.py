a6c10ca4ff370422e8d1ef3f705f16c3a7be4e52
Miguel Angelo Maiopue Guilovica <miguelangelomg2004@gmail.com>
2026-06-18 13:13:06 +0200

    Update message_service.py
    Adding the private messaging method to the services

--- services/message_service.py
+++ services/message_service.py
@@ -57,6 +57,52 @@ class MessageService:
 
         for client_socket in rooms[room]:
 
+
+
+
+    @staticmethod
+    def send_private_message(
+        sender_username,
+        sender_socket,
+        recv_username,
+        private_history, 
+        message,
+        clients
+        ):
+
+        #figuring out if the receiver in connected/ in clients
+        for client in clients:
+            if client["username"] == recv_username:
+                recv_socket = client["socket"]
+                full_message = (
+                    f"{sender_username}: {message}"
+                     )
+
+                # tuple that will identify each private message
+                # sorted makes the key unique regardless of who is sending/ receiving
+                # it's recognized that ideally ID's should had been added in auth used  for the key as names can change, 
+                private_key = tuple(sorted([sender_username, recv_username]))
+
+                #add the private key with value as an array where future messages will be appended
+                if private_key not in private_history:
+                    private_history[private_key] = []
+
+                private_history[private_key].append(full_message)
+                
+                try:
+                 recv_socket.send(full_message.encode())
+                 return "Sent"
+                except:
+                 print(f"Failed to send private message to {recv_username}")
+                 return "Failed"
+
+        try:
+            sender_socket.send(f"User {recv_username} not found".encode())
+        except:
+            print("Failed to notify sender that user not found")
+        return "User not found"
+
+
             client_socket.send(
                 full_message.encode()
             )
