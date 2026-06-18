0e134c9ba2972712242c414fd246cf0d9dd5b6b0
Miguel Angelo Maiopue Guilovica <miguelangelomg2004@gmail.com>
2026-06-18 14:59:31 +0200

    Updated MessageService
    - Added sending private message history method in the service and error handling in all the .send()

--- services/message_service.py
+++ services/message_service.py
@@ -8,10 +8,13 @@ class MessageService:
     ):
 
         if room_name not in message_history:
-
-            client_socket.send(
+            try:
+             client_socket.send(
                 "ERROR: Room does not exist".encode()
             )
+             
+            except:
+             print("Failed to send error to client")
 
             return
 
@@ -35,7 +38,11 @@ class MessageService:
                 f"{room_name}\n"
             )
 
-        client_socket.send(history_text.encode())
+        try:
+           client_socket.send(history_text.encode())
+        except:
+           print("Failed to send history to client")
+        return 
 
     @staticmethod
     def send_message_to_room(
@@ -47,16 +54,39 @@ class MessageService:
         message
     ):
 
+        if sender_socket not in user_rooms:
+         try:
+            sender_socket.send("Error: You are not in any room".encode())
+         except:
+            print("Failed to notify sender about room error")
+         return
+        
         room = user_rooms[sender_socket]
+
+        if room not in rooms:
+         try:
+            sender_socket.send(f"Error: Room '{room}' does not exist".encode())
+         except:
+            print(f"Failed to notify sender about missing room {room}")
+         return
 
         full_message = (
             f"[{room}] {username}: {message}"
         )
 
+         
+        if room not in message_history:
+         message_history[room] = []
         message_history[room].append(full_message)
 
         for client_socket in rooms[room]:
+          try:
+            client_socket.send(full_message.encode())
+          except:
+            print(f"Failed to send message to {client_socket}")
 
+            
+            
 
 
 
@@ -101,8 +131,37 @@ class MessageService:
         except:
             print("Failed to notify sender that user not found")
         return "User not found"
+    
+    
+    @staticmethod
+    def send_priv_history_to_client(private_history, client_socket, requester_username, target_username):
+        
+        private_key = tuple(sorted([requester_username, target_username]))
 
-
-            client_socket.send(
-                full_message.encode()
+        if private_key in private_history:
+           history_text = (
+                f"\n--- Message History "
+                f"for {requester_username} and {target_username} ---\n"
             )
+            
+            # how the message will be sent and printed in the client
+           history_text += "\n".join(
+                private_history[private_key]
+            )
+           history_text += "\n-----------------------\n"
+
+           try:
+            client_socket.send(history_text.encode())
+           except:
+            print("Failed to send private history to client")
+
+        else:
+           try: 
+              client_socket.send(f"No previous messages between you and {target_username}")
+           except:
+              print(f"Failed to notify that there weren't any messages between {requester_username} and {target_username}")
+            
+    
+
+
+            
